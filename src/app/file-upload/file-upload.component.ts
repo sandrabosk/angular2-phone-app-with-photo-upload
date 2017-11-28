bbf58e7db4454666f5884f8e8870c1ea5a0260b7
sandrabosk <sandraboskovic@hotmail.com>
2017-11-27 23:32:52 -0500

    done

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 import { PhoneService } from '../phone.service';
 import { Http, Response } from '@angular/http';
 
+//ElementRef - allows you to select items in body directly, example some div
+
 
 @Component({
   selector: 'app-file-upload',
@@ -26,19 +28,19 @@ constructor(
 
 ngOnInit() {}
 
-
+//here we are creating form outside the angular and using it inside the angular
  submit() {
    console.log("SUBMITTING FORM")
-    let inputEl: HTMLInputElement = this.el.nativeElement.querySelector('#file');
-    let form = new FormData();
-    form.append('file', inputEl.files.item(0));
+    let inputEl: HTMLInputElement = this.el.nativeElement.querySelector('#file'); //#file==> give me the element with the id file
+    let form = new FormData(); //creatingan empty form
+    form.append('file', inputEl.files.item(0)); //attaching the elements
       form.append('name', this.newPhone.name);
       form.append('brand', this.newPhone.brand);
 
       console.log(form);
-
-
-      // this.uploader.uploadAll();
+                                              // this is the route from our express app ('/api/upload')  
+                                              //  |
+      // this.uploader.uploadAll();               |
     return this.http.post(`http://localhost:3000/api/upload/`, form)
       .toPromise()
       .then(apiResponse => apiResponse.json());
